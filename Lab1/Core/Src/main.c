08f2fb4659df34bf5c70800dd8c7f69628af3c37
Huy-Egoist <huy.luuegoist@hcmut.edu.vn>
2024-09-18 21:20:55 +0700

    Test 1

--- Lab1/Core/Src/main.c
+++ Lab1/Core/Src/main.c
@@ -93,6 +93,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  int c = 0;
     /* USER CODE END WHILE */
 	  HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin ) ;
 	  HAL_Delay (1000) ;
