eba6472e31731882299ccd7ad423fdc709363d58
Huy-Egoist <huy.luuegoist@hcmut.edu.vn>
2024-09-18 21:05:20 +0700

    Done

--- Lab1/Core/Src/main.c
+++ Lab1/Core/Src/main.c
@@ -91,13 +91,16 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin, GPIO_PIN_SET);
+  int count = 0;
   while (1)
   {
-    /* USER CODE END WHILE */
-	  HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin ) ;
-	  HAL_Delay (1000) ;
-
-    /* USER CODE BEGIN 3 */
+	  if (count == 0){
+		    HAL_GPIO_TogglePin(GPIOA, LED_RED_Pin);
+		    HAL_GPIO_TogglePin(GPIOA, LED_YELLOW_Pin);
+	  }
+	  count = 1 - count;
+	  HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
@@ -150,14 +153,14 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LED_RED_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin;
+  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_RED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
@@ -176,6 +179,8 @@ void Error_Handler(void)
   __disable_irq();
   while (1)
   {
+	  HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin ) ;
+	  HAL_Delay (1000) ;
   }
   /* USER CODE END Error_Handler_Debug */
 }
